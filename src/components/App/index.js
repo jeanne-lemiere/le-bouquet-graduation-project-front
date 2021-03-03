51166b8150db4bd61437de8e955d85b0e0dec9a7
jeanne-lemiere <jeanne.lemiere.dev@gmail.com>
2021-03-03 15:03:45 +0100

    auth modal working, styled and responsive with static props

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,16 +3,12 @@ import React from 'react';
 
 // == Import
 import './styles.css';
-import Header from '../Header';
-import Footer from '../Footer';
 import AuthModal from '../AuthModal';
 
 // == Composant
 const App = () => (
   <div>
-    <Header isLoggedIn />
     <AuthModal isHidden={false} />
-    <Footer />
   </div>
 );
 
