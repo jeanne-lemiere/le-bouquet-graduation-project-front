a1ab465e5c14bae44af69d327b6a36d8598dfa03
jeanne-lemiere <jeanne.lemiere.dev@gmail.com>
2021-03-04 16:09:40 +0100

    article component completely responsive

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,19 +5,13 @@ import Footer from '../Footer';
 import BouquetSemaine from '../BouquetSemaine';
 import Article from '../Article';
 // == Import
-import reactLogo from './react-logo.svg';
 import './styles.css';
 
 // == Composant
 
-
-
 const App = () => (
-  <div >
-    <Header isLoggedIn={true}/>
-    <BouquetSemaine/>
-    <Article/>
-    <Footer/>
+  <div>
+    <Article />
   </div>
 );
 
--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,35 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './styles.scss';
-import PicArticle from 'src/assets/zoe-schaeffer.jpg';
+import ArticlePic from 'src/assets/zoe-schaeffer.jpg';
 
+const Article = () => (
 
-const Article = ({}) => (
-  
   <div className="article">
-  <div className="article__content">
-    
-  <div className="article__content__text">
-  <h1 className="article__content__text__title">Cultivées avec amour par des passionnés.</h1>
-    <p className="article__content__text__writing">
-      <span className="italic">C’est le bouquet!</span> est né du désir de participer à 
-      <span className="gras"> relocaliser la production et la vente de fleurs</span>, en proposant un outil pour 
-      <span className="gras"> valoriser la filière française, faciliter les échanges</span> 
-        entre producteurs et particuliers, et favoriser l’achat de <span className="gras">fleurs locales et de saison</span>.
-      <br/>
-
-      <br/>
-        Ainsi nous avons conçu une plateforme multi-vendeurs sur laquelle les horticulteurs français pourraient proposer leur production à la vente 
-      <span className="gras"> sans intermédiaire.</span>
-      </p>
-       <p className="article__content__text__link" > <a href="#">Découvrir nos producteurs</a></p>
-   </div>    
-    <img className = "article__content__pic" src={PicArticle} alt="Producer"/>
+    <div className="text-container">
+      <div className="article__text">
+        <h1>Cultivées avec amour par des passionnés.</h1>
+        <p>
+          <i>C’est le bouquet!</i> est né du désir de participer à
+          <em> relocaliser la production et la vente de fleurs</em>, en proposant un outil pour
+          <em> valoriser la filière française, faciliter les échanges</em>
+          entre producteurs et particuliers, et favoriser l’achat de
+          <em> fleurs locales et de saison.</em>
+        </p>
+        <p>Ainsi nous avons conçu une plateforme multi-vendeurs sur laquelle les horticulteurs
+          français pourraient proposer leur production à la vente
+          <em> sans intermédiaire.</em>
+        </p>
+        <a href="#">Découvrir nos producteurs →</a>
+      </div>
+    </div>
+    <div className="picture-container">
+      <img className="article__picture" src={ArticlePic} alt="horticultrice portant un seau de pavots" />
+    </div>
 
   </div>
-  </div>
 
-)
-
+);
 
 export default Article;
