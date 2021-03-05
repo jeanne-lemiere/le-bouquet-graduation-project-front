e480aad808cf4cc05d0b9b75f117384b0f6a804a
jeanne-lemiere <jeanne.lemiere.dev@gmail.com>
2021-03-05 08:34:19 +0100

    added social  media links in footer + some css

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,13 +5,13 @@ import { FiFacebook, FiInstagram } from 'react-icons/fi';
 const Footer = ({}) => (
   <footer className="footer">
     <div className="footer__content">
-      <div className="footer__links footer__links--text">
-        <a href="#">Qui sommes nous?</a>
-        <a href="#">Conditions générales de vente</a>
+      <div className="footer__links">
+        <a className=" footer__links--text" href="#">Qui sommes nous?</a>
+        <a className="footer__links--text" href="#">Conditions générales de vente</a>
       </div>
       <div className="footer__links footer__links--icons">
-        <FiInstagram className="footer__icon" />
-        <FiFacebook className="footer__icon" />
+        <a href="https://www.instagram.com/oclock_io/?hl=fr"><FiInstagram className="footer__icon" /></a>
+        <a href="https://fr-fr.facebook.com/Oclock.io/"><FiFacebook className="footer__icon" /></a>
       </div>
     </div>
   </footer>
